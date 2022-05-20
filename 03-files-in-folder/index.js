2c5448f01426a115945c95986c0e53d38de1e037
Straight To <straighttojanestreet@rs.school>
2022-05-20 11:48:36 +0300

    feat: add 03-files-in-folder solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const { resolve, extname } = require('path');
+const fsp = require('fs/promises');
+
+const statsOfEntry = (dirPath) => (entryName) => {
+  const filePath = resolve(dirPath, entryName);
+  return fsp.stat(filePath).then(stats => ({
+    name: entryName,
+    isDir: stats.isDirectory(),
+    size: stats.size,
+  }));
+}
+
+function readStats(dirPath, entries) {
+  const statsPromises = entries.map(statsOfEntry(dirPath));
+  return Promise.all(statsPromises);
+}
+
+const rootPath = resolve(__dirname, 'secret-folder')
+fsp.readdir(rootPath)
+  .then(entries => readStats(rootPath, entries))
+  .then(items => items.filter(({ isDir }) => !isDir))
+  .then(items => items.map(({ name, size }) => ({ name, ext: extname(name), size})))
+  .then(items => items.forEach(({name, ext, size}) => console.log(`${name} - ${ext} - ${size}`)));
