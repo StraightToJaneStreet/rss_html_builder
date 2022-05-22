94553eea0d01f2dbeca33d3ae2c48831b7018b76
anton13602 <an-kochetov@mail.ru>
2022-05-22 15:14:25 +0300

    feat: 04-copu-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const path = require('path');
+const { readdir, mkdir, rm, copyFile } = require('fs/promises');
 
+async function copyFiles() {
+  
+  const urlFiles = path.join(__dirname, 'files');
+  const urlCopyFile = path.join(__dirname, 'files-copy');
+  await mkdir(`${__dirname}/files-copy`, {recursive: true});
+
+  const readCopyFiles = await readdir(urlCopyFile);
+  for (let copyFile of readCopyFiles) {
+    await rm(`${__dirname}/files-copy/${copyFile}`, {recursive: true});
+  } 
+  
+  const readFiles = await readdir(urlFiles);
+  for (let file of readFiles) {
+    await copyFile (`${__dirname}/files/${file}`, `${__dirname}/files-copy/${file}`);
+  }
+}
+
+copyFiles();
