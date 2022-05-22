71e790cb91ac8ff0d29865226c15456fe0e392f1
anton13602 <an-kochetov@mail.ru>
2022-05-22 22:02:51 +0300

    fix: change links

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,16 +5,16 @@ async function copyFiles() {
   
   const urlFiles = path.join(__dirname, 'files');
   const urlCopyFile = path.join(__dirname, 'files-copy');
-  await mkdir(`${__dirname}/files-copy`, {recursive: true});
+  await mkdir(urlCopyFile, {recursive: true});
 
   const readCopyFiles = await readdir(urlCopyFile);
   for (let copyFile of readCopyFiles) {
-    await rm(`${__dirname}/files-copy/${copyFile}`, {recursive: true});
+    await rm(path.join(urlCopyFile, copyFile), {recursive: true});
   } 
   
   const readFiles = await readdir(urlFiles);
   for (let file of readFiles) {
-    await copyFile (`${__dirname}/files/${file}`, `${__dirname}/files-copy/${file}`);
+    await copyFile(path.join(urlFiles, file), path.join(urlCopyFile, file));
   }
 }
 
