65f9809f0025a1f401e77fd371d195ed07260ebd
anton13602 <an-kochetov@mail.ru>
2022-05-22 22:07:14 +0300

    fix: chanhe links

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -8,12 +8,12 @@ async function mergeFiles() {
   const urlNewCss = path.join(__dirname, 'project-dist');
   const urlReadCss = path.join(__dirname, 'styles');
   
-  const writableStream = fs.createWriteStream(`${urlNewCss}/bundle.css`);
+  const writableStream = fs.createWriteStream(path.join(urlNewCss, 'bundle.css'));
 
   const readCss = await readdir(urlReadCss, {withFileTypes: true});
   for (let cssFile of readCss) {
     if (cssFile.isFile() && cssFile.name.includes('css')) {
-      const urlFileCss = `${urlReadCss}/${cssFile.name}`;
+      const urlFileCss = path.join(urlReadCss, cssFile.name);
       const stream = fs.createReadStream(urlFileCss, {encoding: 'utf-8'});
       stream.on('data', (data) => {
         writableStream.write(data);
